38a54446d3632badd33b6e123f326960ad48d437
sharloteean <sharloteean@gmail.com>
2017-02-14 15:03:26 -0800

    Added sound effects for errors and added HP bar for wild pokemon

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,7 @@
-
-
   var chosenPoke;         // Chosen Pokemon      
-  var chosenMove;               // Chosen Move
+  var chosenMove;         // Chosen Move
   var word;               //word to guesss
-  var guess;             // Guess
+  var guess;              // Guess
   var guesses = [ ];      // Stored guesses
   var lives ;             // Lives
   var counter ;           // Count correct guesses
@@ -13,25 +11,74 @@
   var showLives = document.getElementById("mylives");
   var showUsed = document.getElementById("usedChar");
 
-  
+  function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
 
 window.onload = function play() {
 
-  //choose a word
-  var rng = Math.floor(Math.random() * 7);
+    //choose a number
+    var rng = Math.floor(Math.random() * 7);
+
+  //choose a pokemon and move depending on rng 
     chosenPoke = pokemon[rng];
-    chosenMove = pokeType[rng][Math.floor(Math.random() * 4)];
+    chosenName = chosenPoke.name;
+    chosenMove = chosenPoke.type[Math.floor(Math.random() * 4)];
+
+  //make the words lower case for replacement  
+    chosenName = chosenName.toLowerCase();
+    chosenMove = chosenMove.toLowerCase();
+
+  //change image depending on chosen Pokemon
     var image = document.getElementById("poke");
     image.src = chosenPoke["img"];
-    chosenPoke = chosenPoke["name"].replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/gi, '_');
-    chosenMove = chosenMove.replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/gi, '_');
-    document.getElementById("hangman-space").innerHTML = chosenPoke + ' used ' +chosenMove;
-  //choose a move
-  //  chosenMove = pokeType[Math.floor(Math.random() * pokeType.length)];
 
-    console.log(chosenPoke);
+  //add pokemon cry on appearance
+    var sound = document.getElementById("pokeCry");
+    sound.src = chosenPoke["cry"];
 
-    guesses = [ ];
+  //replace the words with '_' and prints on the screen
+    wordChosenPoke = chosenName.replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/gi, '_');
+    wordChosenMove = chosenMove.replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z/gi, '_');
+    document.getElementById("hangman-space").innerHTML = wordChosenPoke + ' used ' + wordChosenMove;
+  
+  
+    addEventListener("keypress", function(event) {            //Listens for character press
+    var guess = String.fromCharCode(event.charCode);          //Stores key pressed  
+      if (guesses.indexOf(guess) == -1){                      //If guess is not in the array of guesses 
+                guesses.push(guess);                          //Add to array 
+                sound.src = hitSound;
+      } 
+      else{                                                    //Otherwise display error message
+      sound.src = error;
+      document.getElementById("messages").innerHTML = "You've already guessed that letter!";
+      }
+    document.getElementById("guesses").innerHTML = guesses;     //Display already used letters
+    
+          for(var i = 0; i<guesses.length; i++){
+            for (var j=0; j<chosenName.length;j++){
+              if(chosenName[j]==guesses[i]){
+                wordChosenPoke = wordChosenPoke.substr(0,j) + guesses[i] + wordChosenPoke.substr(j+1);
+                document.getElementById("hangman-space").innerHTML = wordChosenPoke + ' used ' + wordChosenMove;
+              }              
+            }
+            for (var k=0;k<chosenMove.length;k++){
+              if(chosenMove[k]==guesses[i]){
+                wordChosenMove = wordChosenMove.substr(0,k) + guesses[i] + wordChosenMove.substr(k+1);
+                document.getElementById("hangman-space").innerHTML = wordChosenPoke + ' used ' + wordChosenMove;
+              }                   
+            }
+          }
+
+   
+  });
+
+
+  
+  
+
+    
     lives = 10;
     counter = 0
     space = 0;
--- assets/javascript/helper.js
+++ assets/javascript/helper.js
@@ -1,49 +1,7 @@
 var pokemon;            // Selected pokemon 
 var pokeType;           // Selected type
-
-var squirtle = {
-	name: "Squirtle",
-	type: "water",
-	img: "assets/images/squirtle.gif"
-}
-
-var cyndaquil = {
-	name: "Arcanine",
-	type: "fire",
-	img: "assets/images/arcanine.gif"
-}
-
-var bellossom = {
-	name: "Bellosom",
-	type: "grass",
-	img: "assets/images/bellossom.gif"
-}
-
-var pikachu = {
-	name: "Pikachu",
-	type: "electric",
-	img: "assets/images/pikachu.gif"
-}
-
-var wigglytuff = {
-	name: "Wigglytuff",
-	type: "normal",
-	img: "assets/images/wigglytuff.gif"
-}
-
-var gardevoir = {
-	name: "Gardevoir",
-	type: "psychic",
-	img: "assets/images/gardevoir.gif"
-}
-
-var groudon = {
-	name: "Groudon",
-	type: "ground",
-	img: "assets/images/groudon.gif"
-}
-
-pokemon = [squirtle, cyndaquil, bellossom, pikachu, wigglytuff, gardevoir, groudon];
+var hitSound = "assets/sounds/hitsound.wav";
+var error = "assets/sounds/error.mp3";
 
 var electric = ["Thunderbolt", "Thunder", "Volt Switch", "Volt Tackle"];
 
@@ -61,11 +19,60 @@ var ground = ["Earthquake", "Mud Bomb", "Dig", "Fissure"];
 
 var psychic = ["Psychic", "Confusion", "Future Sight", "Zen Headbutt"];
  
+var squirtle = {
+	name: "Squirtle",
+	type: water,
+	img: "assets/images/squirtle.gif",
+	cry: "assets/sounds/squirtle.mp3"
+}
 
+var arcanine = {
+	name: "Arcanine",
+	type: fire,
+	img: "assets/images/arcanine.gif",
+	cry: "assets/sounds/arcanine.mp3"
+}
+
+var bellossom = {
+	name: "Bellosom",
+	type: grass,
+	img: "assets/images/bellossom.gif",
+	cry: "assets/sounds/bellossom.mp3"
+}
+
+var pikachu = {
+	name: "Pikachu",
+	type: electric,
+	img: "assets/images/pikachu.gif",
+	cry: "assets/sounds/pikachu.mp3"
+}
+
+var wigglytuff = {
+	name: "Wigglytuff",
+	type: normal,
+	img: "assets/images/wigglytuff.gif",
+	cry: "assets/sounds/wigglytuff.mp3"
+}
+
+var gardevoir = {
+	name: "Gardevoir",
+	type: psychic,
+	img: "assets/images/gardevoir.gif",
+	cry: "assets/sounds/gardevoir.mp3"
+}
+
+var groudon = {
+	name: "Groudon",
+	type: ground,
+	img: "assets/images/groudon.gif",
+	cry: "assets/sounds/groudon.mp3"
+}
+
+pokemon = [squirtle, arcanine, bellossom, pikachu, wigglytuff, gardevoir, groudon];
 
 pokeType = [water, fire, grass, electric, normal, psychic, ground];
-//			bug, ground, rock, ice, poison, ghost, psychic, dark,
-//			steel, dragon, fairy, fighting];
+		//	bug, ground, rock, ice, poison, ghost, psychic, dark,
+		//	steel, dragon, fairy, fighting];
 
 
 
