42761695702e38a7515ed90cb1c25301cb07885f
sharloteean <sharloteean@gmail.com>
2017-02-15 17:39:37 -0800

    Added caught counter and changed JS to jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,22 @@
   var word;               //word to guesss
   var guess;              // Guess
   var guesses = [ ];      // Stored guesses
-  var lives ;             // Lives
-  var counter ;           // Count correct guesses
-  var space;              // Number of spaces in word '-'
+  var caught =0;             //number of pokemon caught
+  var image = document.getElementById("poke");
+  var sound = document.getElementById("pokeCry");
+  var delayMillis = 1000; //1 second
+  var hits;               //counts amount of spaces left
 
    // Get elements
   var showLives = document.getElementById("mylives");
   var showUsed = document.getElementById("usedChar");
 
-  function toTitleCase(str)
-{
-    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
+  window.onload = function play() {
 
-window.onload = function play() {
-
-    //choose a number
+//Create a new pokemon
+  function newPoke(){
+  
+  //choose a number
     var rng = Math.floor(Math.random() * 7);
 
   //choose a pokemon and move depending on rng 
@@ -31,11 +31,10 @@ window.onload = function play() {
     chosenMove = chosenMove.toLowerCase();
 
   //change image depending on chosen Pokemon
-    var image = document.getElementById("poke");
+
     image.src = chosenPoke["img"];
 
   //add pokemon cry on appearance
-    var sound = document.getElementById("pokeCry");
     sound.src = chosenPoke["cry"];
 
   //replace the words with '_' and prints on the screen
@@ -48,52 +47,63 @@ window.onload = function play() {
     lifePerc= lives/lives * 100;
     $("#enemy-hp").html(lifePerc+ "%");
     $("#my-hp").html(lifePerc + "%");
+  }
+    
+  newPoke();
+  $("#messages").html("<p>Caught: </p>"+ caught);
 
-
-
+    //Stores key pressed  
     addEventListener("keypress", function(event) {            //Listens for character press
-    var guess = String.fromCharCode(event.charCode);          //Stores key pressed  
+    var guess = String.fromCharCode(event.charCode);          
       if (guesses.indexOf(guess) == -1){                      //If guess is not in the array of guesses 
                 guesses.push(guess);                          //Add to array 
                 sound.src = hitSound;
+                  $("#messages").html("<p>Caught: </p>"+ caught);
+
       } 
       else {                                                    //Otherwise display error message
       sound.src = error;
-      document.getElementById("messages").innerHTML = "You've already guessed that letter!";
+      $("#messages").html("You've already guessed that letter!");
       }
     
-    document.getElementById("guesses").innerHTML = guesses;     //Display already used letters
+    //Display already used letters
+    document.getElementById("guesses").innerHTML = guesses;     
     
     //replaces the letters in the spaces with the guessed letter
-          for(var i = 0; i<guesses.length; i++){
-            for (var j=0; j<chosenName.length;j++){
-              if(chosenName[j]==guesses[i]){
-                wordChosenPoke = wordChosenPoke.substr(0,j) + guesses[i] + wordChosenPoke.substr(j+1);
-                wordChosenPoke = wordChosenPoke.toUpperCase();
-                document.getElementById("hangman-space").innerHTML = wordChosenPoke + ' used ' + wordChosenMove;
-              }              
-            }
-            for (var k=0;k<chosenMove.length;k++){
-              if(chosenMove[k]==guesses[i]){
-                wordChosenMove = wordChosenMove.substr(0,k) + guesses[i] + wordChosenMove.substr(k+1);
-                wordChosenMove = wordChosenMove.toUpperCase();
-                document.getElementById("hangman-space").innerHTML = wordChosenPoke + ' used ' + wordChosenMove;
-              }                   
-            }
-          }
+    for(var i = 0; i<guesses.length; i++){
+      for (var j=0; j<chosenName.length;j++){
+        if(chosenName[j]==guesses[i]){
+          wordChosenPoke = wordChosenPoke.substr(0,j) + guesses[i] + wordChosenPoke.substr(j+1);
+          wordChosenPoke = wordChosenPoke.toUpperCase();
+          }              
+        }
+      for (var k=0;k<chosenMove.length;k++){
+        if(chosenMove[k]==guesses[i]){
+          wordChosenMove = wordChosenMove.substr(0,k) + guesses[i] + wordChosenMove.substr(k+1);
+          wordChosenMove = wordChosenMove.toUpperCase();
+          }                   
+        }
+    }
 
+    //prints the letters in the div 
+    $("#hangman-space").html(wordChosenPoke + ' used ' + wordChosenMove);
+
+    hits = (wordChosenPoke.split("_").length - 1) + (wordChosenMove.split("_").length - 1);
+    console.log(hits);
      if (wordChosenMove == chosenMove.toUpperCase()){
-        console.log(wordChosenMove);
+          console.log(wordChosenMove);
+          caught++;
+          guesses = [];
+          $("#guesses").html(guesses);
+          setTimeout(function() {
+          newPoke();
+          }, delayMillis);
+          $("#messages").html("<p>Caught: </p>"+ caught);
      }
    
   });
 
   
-  
 
-    
-    lives = 10;
-    counter = 0
-    space = 0;
 
-  }
+}
